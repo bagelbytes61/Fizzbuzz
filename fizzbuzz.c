82423b9d8e99264e7cd986a7bfadbba7bac1f988
bagelbytes61 <Renthalkx@gmail.com>
2019-03-30 01:23:39 -0400

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,22 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main(void) {
+    for (unsigned i = 1; i <= 100; ++i) {
+        unsigned fizz = ~!(i % 3) + 1 << 16;;
+        unsigned buzz = ~!(i % 5) + 1 & 0x0000ffff;
+
+        fizz = ~(fizz >> 31) + 1;
+        buzz = ~(buzz >> 15) + 1;
+
+        unsigned fizzbuzz = fizz & buzz;
+
+        printf((uintptr_t)"%s\n" & (fizz | buzz) | (uintptr_t)"%u\n" & ~(fizz | buzz),
+            (uintptr_t)"fizz" & ((fizz ^ buzz) & ~buzz) |
+            (uintptr_t)"buzz" & ((buzz ^ fizz) & ~fizz) |
+            (uintptr_t)"fizzbuzz" & fizzbuzz |
+            i & ~(fizz | buzz));
+    }
+
+    return 0;
+}

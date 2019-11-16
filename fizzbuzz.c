4e83ee677ad0096d2ec523e940d1f38f77e2f2c3
Jeremy Lyda <Renthalkx@gmail.com>
2019-11-15 23:23:27 -0500

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,16 +1,17 @@
+#include <inttypes.h>
 #include <limits.h>
 #include <stdio.h>
 
 int main(void) {
     for (unsigned i = 1; i <= 100; ++i) {
-        unsigned fizz = ~!(i % 3) + 1;
-        unsigned buzz = ~!(i % 5) + 1;
+        unsigned fizz = ~(unsigned)!(i % 3) + 1;
+        unsigned buzz = ~(unsigned)!(i % 5) + 1;
 
         unsigned fizzbuzz = fizz & buzz;
 
-        printf((uintptr_t)"%s\n" & (fizz | buzz) | (uintptr_t)"%u\n" & ~(fizz | buzz),
-            (uintptr_t)"fizz" & ((fizz ^ buzz) & ~buzz) |
-            (uintptr_t)"buzz" & ((buzz ^ fizz) & ~fizz) |
+        printf((uintptr_t)"%s\n" & (fizz | buzz) | (uintptr_t)"%"PRIuPTR"\n" & ~(fizz | buzz),
+            (uintptr_t)"fizz" & (fizz & ~buzz) |
+            (uintptr_t)"buzz" & (buzz & ~fizz) |
             (uintptr_t)"fizzbuzz" & fizzbuzz |
             i & ~(fizz | buzz));
     }
